dfd7e7b99ec86ddf3136b6a9ac876332248e1e2a
Agnaldo Cavaleiro Costa <guibrixton@MacBook-Pro-de-Agnaldo.local>
2024-05-18 20:16:59 +0200

    Update

--- Jenkins-parse-data.py
+++ Jenkins-parse-data.py
@@ -8,7 +8,7 @@ url = sys.argv[1]
 path = sys.argv[2]
 
 def extract_jenkinsfile_data(url, path):
-  jenkinsfile_url = f"{url}/raw/master/{path}"
+  jenkinsfile_url = f"{url}/raw/master/{path}/Jenkinsfile"
   response = requests.get(jenkinsfile_url)
   jenkinsfile_content = response.text
 
@@ -44,8 +44,8 @@ def extract_jenkinsfile_data(url, path):
   print(json.dumps(data, indent=4))
 
 # Example usage
-# url = "https://github.com/agill17/Infrastructure-as-Code.git"
-# path = "path/to/jenkinsfile"
+url = "https://github.com/agill17/Infrastructure-as-Code.git"
+path = "Jenkinsfile/102/jenkinsfile"
 extract_jenkinsfile_data(url, path)
 
 
